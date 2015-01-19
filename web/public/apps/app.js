89ebadb20e3306873647927964c810b4e52f7d40
Anthony <anthony@iamcompany.net>
2015-01-20 00:59:58 +0900

    앱 요청시 root 정보 설정.

--- web/public/apps/app.js
+++ web/public/apps/app.js
@@ -5,7 +5,57 @@
 var app = angular.module('myApp', [
     'ui.router', 
     'content',
-]);
+], function($httpProvider){
+    $httpProvider.interceptors.push(function($q) {
+        return {
+            'request': function(config) {
+                // same as above
+                var html = /^\/api\//;
+                if(html.test(config.url)){
+                    config.url  = '..'+config.url;
+                }
+
+                return config || $q.when(config);
+            },
+
+            'response': function(response) {
+                // same as above
+                return response;
+            },
+            responseError: function (rejection) {
+                return rejection;
+            }
+        };
+    });
+});
+
+
+
+var angularApp = angular.module("webapp", [
+        'ui.bootstrap',
+        'ui.calendar',
+        'ui.select'],
+    function($httpProvider){
+        $httpProvider.interceptors.push(function($q) {
+            return {
+                'request': function(config) {
+                    // same as above
+                    return config || $q.when(config);
+                },
+
+                'response': function(response) {
+                    // same as above
+                    return response;
+                },
+                responseError: function (rejection) {
+                    modalAlert(rejection.statusText, "alert-danger");
+                    return rejection;
+                }
+            };
+        });
+
+    });
+
 
 app.run(function($rootScope, func){
     $rootScope.$fn = func;
